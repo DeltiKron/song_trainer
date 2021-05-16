e70f9c2edf6751b200374deb11bacbdd541b9140
DeltiKron <cfmschaffer@hotmail.de>
2021-05-16 18:21:22 +0200

    adding import to song list

--- app/src/main/java/com/example/song_trainer/SongListActivity.java
+++ app/src/main/java/com/example/song_trainer/SongListActivity.java
@@ -1,27 +1,28 @@
 package com.example.song_trainer;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,6 +33,7 @@ import static com.android.volley.toolbox.Volley.newRequestQueue;
 public class SongListActivity extends AppCompatActivity implements SongsAdapter.onSongListener {
     private static final String TAG = "SongListActivity";
     private List<Song> mSongs;
+    private String m_Text = "";
 
     private void update() {
         // Lookup the recyclerview in activity layout
@@ -42,15 +44,65 @@ public class SongListActivity extends AppCompatActivity implements SongsAdapter.
         mSongs = songDB.songDAO().getSongList();
 
         // Create adapter passing in the sample user data
-        SongsAdapter adapter = new SongsAdapter(mSongs,this);
+        SongsAdapter adapter = new SongsAdapter(mSongs, this);
         // Attach the adapter to the recyclerview to populate items
         rvSongs.setAdapter(adapter);
         // Set layout manager to position the items
         rvSongs.setLayoutManager(new
                 LinearLayoutManager(this));
         // That's all!
+    }
 
-        this.importSongsFromJSON("http://192.168.2.101:5000/songs/export_songs",songDB);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+
+            case R.id.action_import:
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Import from JSON endpoint");
+                // I'm using fragment here so I'm using getView() to provide ViewGroup
+                // but you can provide here any other instance of ViewGroup from your Fragment / Activity
+                View viewInflated = LayoutInflater.from(this).inflate(R.layout.string_query_dialog,findViewById(android.R.id.content),false);
+                // Set up the input
+                final EditText input = (EditText) viewInflated.findViewById(R.id.input);
+                // input.setText("192.168.2.132:5000/songs/export_songs");
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                builder.setView(viewInflated);
+
+                // Set up the buttons
+                builder.setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                    dialog.dismiss();
+                    m_Text = input.getText().toString();
+                    this.importSongsFromJSON(m_Text,SongDatabase.getInstance(this));
+
+                });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+                SongDatabase songDB = SongDatabase.getInstance(this);
+                this.importSongsFromJSON(m_Text,songDB);
+
+            default:
+                return true;
+        }
     }
 
     @Override
@@ -84,18 +136,22 @@ public class SongListActivity extends AppCompatActivity implements SongsAdapter.
 
     @Override
     public void onSongClick(int position) {
-        Log.d(TAG,"onSongClick clicked at position "+String.format("%3d",position));
+        Log.d(TAG, "onSongClick clicked at position " + String.format("%3d", position));
         Song song = mSongs.get(position);
         Toast.makeText(getApplicationContext(), song.title, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, PlaySongActivity.class);
-        intent.putExtra("songId",song.songId);
+        intent.putExtra("songId", song.songId);
         this.startActivity(intent);
     }
 
-    public void importSongsFromJSON(String url,SongDatabase db){
+    public void importSongsFromJSON(String url, SongDatabase db) {
         // Instantiate the RequestQueue.
         RequestQueue queue = newRequestQueue(this);
 
+
+        if(!url.startsWith("http")){
+            url = String.format("http://%s",url);
+        }
 // Request a string response from the provided URL.
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url,
                 response -> {
@@ -104,17 +160,23 @@ public class SongListActivity extends AppCompatActivity implements SongsAdapter.
                         Log.d("JsonArray", response.toString());
                         for (int i = 0; i < response.length(); i++) {
                             JSONObject entry = response.getJSONObject(i);
-                            Song song =  Song.from_json(entry);
+                            Song song = Song.from_json(entry);
                             Log.d("title", song.title);
                             boolean exists = db.songDAO().songExists(song.title, song.artist);
                             Log.d("exists", String.valueOf(exists));
-                            if (!exists){
+                            if (!exists) {
                                 db.songDAO().insertSong(song);
                             }
                         }
+
                     } catch (JSONException e) {
+                        Snackbar.make( findViewById(android.R.id.content), "Import Failed!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+
                         e.printStackTrace();
                     }
+                    Snackbar.make( findViewById(android.R.id.content), "Import Successful!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
 
 /*
                         try {
